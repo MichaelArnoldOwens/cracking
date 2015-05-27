78ffea3fc4afa0e4f770149abb185a1c75dee91a
Michael Owens <mowens@ucdavis.edu>
2015-05-26 18:51:21 -0700

    added comments

--- src/cracking/test.java
+++ src/cracking/test.java
@@ -4,14 +4,20 @@ import java.util.*;
 
 public class test {
 
+	//given a string, determines if all characters in it are unique or not
+	//assumes that we are using ascii character set
 	public static boolean isUnique(String str){
 		
+		//creating an array to check if that character has been read before
+		//array is auto initialized to false
 		boolean char_set[] = new boolean[256];
 		
+		//since we are using ascii, the max length of string for all characters to be unique can only be 256
 		if(str.length() > 256) {
 			return false;
 		}
 		
+		//loop through checking each character if it has been read before
 		for(int i = 0; i < str.length(); i++){
 			int letter = str.charAt(i);
 			if(char_set[letter] == true) {
